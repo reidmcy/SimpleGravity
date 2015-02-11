45e6c3b5d75c73aec71a95f7be392668bc3d8bf9
Reid <Reid@v1020-wn-141-153.campus-dynamic.uwaterloo.ca>
2015-02-11 16:02:58 -0500

    Initial Commit

---
+++ SimpleGravity/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SimpleGravity
+//
+//  Created by Reid on 2015-02-11.
+//  Copyright (c) 2015 Reid. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
